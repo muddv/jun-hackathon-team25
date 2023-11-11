fdb4c5b6a7266dc574ab6a1f5826385d0a324329
misha <dedovmu@gmail.com>
2023-11-11 11:04:28 +0300

    Add grid

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,28 @@
-const canvas = document.querySelector<HTMLCanvasElement>("#app")!
+const canvas = document.querySelector<HTMLCanvasElement>("#hehe")!
 const ctx = canvas.getContext("2d")!
 
 function main() {
+  drawGrid();
+}
 
+function drawGrid() {
+  let boxWidth = 800
+  let boxHeight = 800
+  let boxPadding = 10
+  let move = 0.5
+  let tileSize = 50
+  let color = "0x000000"
+  for (let x = 0; x <= boxWidth; x += tileSize) {
+    ctx.moveTo(move + x + boxPadding, boxPadding)
+    ctx.lineTo(move + x + boxPadding, boxHeight + boxPadding)
+  }
+
+  for (let x = 0; x <= boxHeight; x += tileSize) {
+    ctx.moveTo(boxPadding, move + x + boxPadding)
+    ctx.lineTo(boxWidth + boxPadding, move + x + boxPadding)
+  }
+  ctx.strokeStyle = color
+  ctx.stroke()
 }
 
 main()
-
