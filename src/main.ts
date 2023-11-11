c72bcf00ba1c89501cbe325bf8f7f887b969b045
misha <dedovmu@gmail.com>
2023-11-11 10:37:35 +0300

    Remove three

--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,9 @@
-import {Scene, PerspectiveCamera, WebGLRenderer, BoxGeometry, MeshBasicMaterial, Mesh} from 'three'
-import { OrbitControls } from 'three/examples/jsm/Addons.js'
-
-let scene: Scene, camera: PerspectiveCamera, renderer: WebGLRenderer, controls: OrbitControls 
-
-let canvas = document.querySelector<HTMLDivElement>("#app")!
+const canvas = document.querySelector<HTMLCanvasElement>("#app")!
+const ctx = canvas.getContext("2d")!
 
 function main() {
-  init()
-  drawGeometry()
-  animate()
-}
 
-function init() {
-  scene = new Scene()
-  camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-  renderer = new WebGLRenderer({canvas})
-  controls = new OrbitControls(camera, renderer.domElement)
-  renderer.setPixelRatio(window.devicePixelRatio)
-  renderer.setSize(window.innerWidth, window.innerHeight)
-}
-
-function drawGeometry() {
-  const geometry = new BoxGeometry(1, 1, 1)
-  const material = new MeshBasicMaterial({color: 0xf05400})
-  const cube = new Mesh(geometry, material)
-  scene.add(cube)
-  camera.position.z = 5;
-}
-
-function animate() {
-  requestAnimationFrame(animate)
-  controls.update()
-
-  renderer.render(scene, camera)
 }
 
 main()
-
 
