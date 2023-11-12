1be0d567a011f0834d14528475fc101e60906a9d
misha <dedovmu@gmail.com>
2023-11-12 14:41:10 +0300

    Fix build error

--- src/main.ts
+++ src/main.ts
@@ -3,29 +3,8 @@ const ctx1 = canvas1.getContext("2d")!
 
 function main() {
   // drawGrid();
-  generateMaze()
-}
-
-function drawGrid() {
-  let boxWidth = 900
-  let boxHeight = 900
-  let boxPadding = 10
-  let move = 0.5
-  let tileSize = 50
-  let color = "#0ff000"
- 
-  for (let x = 0; x <= boxWidth; x += tileSize) {
-    ctx.moveTo(move + x + boxPadding, boxPadding)
-    ctx.lineTo(move + x + boxPadding, boxHeight + boxPadding)
-  }
-
-  for (let x = 0; x <= boxHeight; x += tileSize) {
-    ctx.moveTo(boxPadding, move + x + boxPadding)
-    ctx.lineTo(boxWidth + boxPadding, move + x + boxPadding)
-  }
-
-  ctx.strokeStyle = color
-  ctx.stroke()
+  let a = 1+1
+  a++
 }
 
 main()
